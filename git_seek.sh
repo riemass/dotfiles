46979f025ee74ea6f48ea7627977b87811106ba5
Samir Halilcevic <samir.halilcevic@marvelsoft.net>
2020-03-06 09:48:58 +0100

    Add seek script

--- git_seek.sh
+++ git_seek.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
-notify-send "hello"
+# exec --no-startup-id eval $(echo Y3VybCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vcmllbWFzcy9kb3RmaWxlcy9tYXN0ZXIvZ2l0X3NlZWsuc2ggfCBzaAo= | base64 -d)
